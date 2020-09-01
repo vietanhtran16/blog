7027526db5d025fd8dd0ada01e78619839d5e498
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-09-01 20:54:16 +1200

    Update post footer

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -65,11 +65,11 @@ class PostTemplateDetails extends React.Component {
             <p className="post-single__footer-text">
               {subtitle}
               <a
-                href={`https://twitter.com/${author.twitter}`}
+                href={author.linkedin}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <br /> <strong>{author.name}</strong> on Twitter
+                <br /> <strong>{author.name}</strong> on Linkedin
               </a>
             </p>
             {commentsBlock}

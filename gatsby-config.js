1c2745948f84bd778c0c54a0a3ac1b8f59cd651a
Viet Anh Tran <tranvietanh1996@gmail.com>
2020-09-01 20:37:00 +1200

    Update gg analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-176899668-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',

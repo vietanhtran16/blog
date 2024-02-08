bdee47ddad2763262a7a97cc925ccf6e68bf7102
Viet Anh Tran <viet.anhtran@cultureamp.com>
2024-02-09 00:11:42 +1100

    Blah

--- scripts/set-up-git.sh
+++ scripts/set-up-git.sh
@@ -2,3 +2,4 @@
 
 git config --global user.name vietanhtran16
 git config --global user.email "tranvietanh1996@gmail.com"
+git remote set-url origin https://vietanhtran16:$ACCESS_TOKEN@github.com/vietanhtran16/vietanhtran16.github.io.git
